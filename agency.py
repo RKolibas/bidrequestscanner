24ccf52327ba0ee1fd9fae60318cafb55940f41a
RKolibas <50153916+RKolibas@users.noreply.github.com>
2021-01-30 09:29:11 -0500

    Add files via upload

---
+++ agency.py
@@ -0,0 +1,7 @@
+class Agency:
+    def __init__(self, name, site, text_file, tag1, tag2):
+        self.name = name
+        self.site = site
+        self.text_file = text_file
+        self.tag1 = tag1
+        self.tag2 = tag2
